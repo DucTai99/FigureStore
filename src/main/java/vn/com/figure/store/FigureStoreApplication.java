ea8c72e4598a48d54265ad7ffa9bfd86cbb718b1
Duc Tai99 <ductaiit99@gmail.com>
2022-10-23 23:19:37 +0700

    Inital setup for Spring Boot

---
+++ src/main/java/vn/com/figure/store/FigureStoreApplication.java
@@ -0,0 +1,13 @@
+package vn.com.figure.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigureStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigureStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/figure/store/FigureStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.figure.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigureStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
